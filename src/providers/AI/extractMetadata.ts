c1bad76b828a7f1eddf0c5ae6a47e8921e80ab7e
yansb <yansbarreiro@gmail.com>
2024-10-26 17:48:44 -0300

    feat: improve metadata prompt

--- src/providers/AI/extractMetadata.ts
+++ src/providers/AI/extractMetadata.ts
@@ -4,10 +4,12 @@ import { model } from "./model.js";
 
 
 const taggingPrompt = ChatPromptTemplate.fromTemplate(
-  `Extrai a informação desejada de monografias do ensino superior.
+  `Extraia apenas as informações contidas no texto de monografias do ensino superior.
 
-Só extraia as propriedades mencionadas na função 'Classification'.
-Não invente metadados apenas extraia os que estão contidos no texto.
+  Regras:
+  - Extraia apenas as propriedades mencionadas na função 'Classification'.
+  - Não invente metadados; extraia apenas os que estão presentes no texto.
+  - Se palavras-chave não estiverem presentes no texto, deixe o campo "palavras-chave" em branco.
 
 Texto:
 {input}
@@ -15,13 +17,13 @@ Texto:
 );
 
 const dadosSchema = z.object({
-  titulo: z.string().describe("O titulo do trabalho"),
+  titulo: z.string().describe("O título do trabalho"),
   discente: z.string().describe("Nome do aluno que foi o autor do trabalho"),
-  orientador: z.string().describe("Nome do orientador responsavel por orientar o aluno pelo trabalho"),
+  orientador: z.string().describe("Nome do orientador responsável por orientar o aluno pelo trabalho"),
   resumo: z.string().describe("Resumo do trabalho escrito pelo autor"),
-  palavrasChave: z.array(z.string()).describe("Array com as palavras-chave definidas pelo autor do texto. Caso o autor não tenha definido palavras-chave deixe em branco"),
+  palavrasChave: z.array(z.string()).describe("Array com as palavras-chave definidas pelo autor do texto. Se o texto não contiver palavras-chave, deixe o array vazio"),
   ano: z.number().describe("Ano em que foi publicada a monografia")
-}).describe("Metadados extraidos de uma monografia");
+}).describe("Metadados extraídos de uma monografia");
 
 const modelWithStructuredOutput = model.withStructuredOutput(dadosSchema, {
   name: 'dados'
