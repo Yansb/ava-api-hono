8f9ab24977150a454a9014d93d01c3ee99ece0e1
yansb <yansbarreiro@gmail.com>
2024-11-21 12:47:53 -0300

    fix: change gpt model

--- src/providers/AI/model.ts
+++ src/providers/AI/model.ts
@@ -3,5 +3,5 @@ import { ChatOpenAI } from "@langchain/openai";
 export const model = new ChatOpenAI({
   openAIApiKey: process.env.OPENAI_API_KEY,
   temperature: 0,
-  model: 'gpt-3.5-turbo-0125'
+  model: 'gpt-4o-mini'
 })
